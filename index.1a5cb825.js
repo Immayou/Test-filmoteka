262a5d857ba645a0b50e71fd1b3dc839c08cef71
Immayou <98358870+Immayou@users.noreply.github.com>
2022-09-25 16:07:12 +0000

    Deploying to gh-pages from @ Immayou/Test-filmoteka@a4477233c02ec941f68a8652058c3c194910e697 🚀

---
+++ index.1a5cb825.js
@@ -0,0 +1,2 @@
+const e=document.forms["search-form"],n=e.elements.search_input;console.log(e),e.addEventListener("submit",(function(e){e.preventDefault();const o=n.value;console.log(o)}));
+//# sourceMappingURL=index.1a5cb825.js.map
