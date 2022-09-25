2df0e69961a0baac482a81205e4664244b25290e
Immayou <98358870+Immayou@users.noreply.github.com>
2022-09-25 21:43:04 +0000

    Deploying to gh-pages from @ Immayou/Test-filmoteka@c8496cdc43fec6e9f06f521c6ac68dd3ba0fdc42 🚀

--- index.b5f56fc9.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.forms["search-form"],n=e.elements.search_input;console.log(e),e.addEventListener("submit",(function(e){e.preventDefault();var o=n.value;console.log(o)}))}();
-//# sourceMappingURL=index.b5f56fc9.js.map
