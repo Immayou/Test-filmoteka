c8496cdc43fec6e9f06f521c6ac68dd3ba0fdc42
Yuliya Zozulya <djulia@ukr.net>
2022-09-26 00:42:42 +0300

    Функція

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
+import FilmsDataApiSearch from './js/dataRequest';
+
 const formInput = document.forms['search-form']
 const input = formInput.elements['search_input']
-console.log(formInput)
+const films_box = document.querySelector('.films_box')
 
-formInput.addEventListener('submit', onRenderInput)
+const filmsDataApiSearch = new FilmsDataApiSearch()
 
-function onRenderInput (evt) {
+formInput.addEventListener('submit', onRanderDataRequestBtn)
+
+async function onRanderDataRequestBtn (evt) {
 evt.preventDefault()
+filmsDataApiSearch.query = input.value
+try {
+    const getDataRequest = await filmsDataApiSearch.request().then(createMarkup)
 
-    const value = input.value
-    console.log(value)
+  } catch (error) {
+    console.log('1')
+  }
+
 }
+
+function createMarkup (films) {
+    // const vote = vote_average
+    // const two = Number(vote).toFixed(1)
+
+    const template = films.map(({poster_path, popularity, vote_average, vote_count, original_title, genres}) => {
+        console.log(popularity)})
+
+    // const template = films.map(
+    //     ({poster_path, popularity, vote_average, vote_count, original_title, genres}) => 
+    //     {return `<div class="film-card">
+    //     <img src="${poster_path}" alt="${overview}" loading="lazy" />
+    //     <div class="info">
+    //       <p class="info-item">
+    //         <b>Likes:</b>
+    //         <span class="info-item-value">${popularity}</span>
+    //       </p>
+    //       <p class="info-item">
+    //         <b>Views:</b>
+    //         <span class="info-item-value">${vote}</span>
+    //       </p>
+    //       <p class="info-item">
+    //         <b>Comments:</b>
+    //         <span class="info-item-value">${vote}</span>
+    //       </p>
+    //       <p class="info-item">
+    //         <b>Downloads:</b>
+    //         <span class="info-item-value">${original_title}</span>
+    //       </p>
+    //     </div>
+    //   </div>`}).join('')
+    //   films_box.insertAdjacentHTML('beforeend', template)
+
+  }
 
---
+++ src/js/dataRequest.js
@@ -0,0 +1,29 @@
+export default class FilmsDataApiSearch {
+    constructor () {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    async request () {
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=f19c93a33a6f9456f5109701414d5b41&query=${this.searchQuery}&page=${this.page}`);
+        const films = await response.json();
+        return films.results;
+    } 
+    
+    incrementPage() {
+        this.page += 1; 
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query () {
+        return this.searchQuery;
+    }
+
+    set query (newQuery) {
+        return this.searchQuery = newQuery
+    }
+
+}
